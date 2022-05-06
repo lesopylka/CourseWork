2bd30d7a3fabcf369f7dd911b6f9d9f4aad17a0a
Alina <99533520+lesopylka@users.noreply.github.com>
2022-05-06 19:11:13 +0300

    Add files via upload

---
+++ SQL Server Management Studio/zali.sql
@@ -0,0 +1,106 @@
+USE [museum]
+GO
+SET IDENTITY_INSERT [dbo].[Zali] ON 
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (1, N'Живопись и скульптура XVIII века', 40)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (2, N'Живопись и скульптура XVIII века', 48)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (3, N'Живопись и скульптура XVIII века', 16)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (4, N'Живопись и скульптура XVIII века', 2)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (5, N'Живопись и скульптура XVIII века', 36)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (6, N'Живопись и скульптура XVIII века', 25)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (7, N'Живопись и скульптура XVIII века', 21)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (8, N'Живопись и скульптура первой половины XIX века', 41)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (9, N'Живопись и скульптура первой половины XIX века', 30)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (10, N'Живопись и скульптура первой половины XIX века', 50)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (11, N'Живопись и скульптура первой половины XIX века', 9)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (12, N'Живопись и скульптура первой половины XIX века', 6)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (13, N'Живопись и скульптура первой половины XIX века', 1)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (14, N'Живопись и скульптура первой половины XIX века', 27)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (15, N'Живопись и скульптура первой половины XIX века', 10)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (16, N' Живопись и скульптура второй половины XIX века', 8)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (17, N' Живопись и скульптура второй половины XIX века', 23)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (18, N' Живопись и скульптура второй половины XIX века', 3)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (19, N' Живопись и скульптура второй половины XIX века', 29)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (20, N' Живопись и скульптура второй половины XIX века', 12)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (21, N' Живопись и скульптура второй половины XIX века', 19)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (22, N' Живопись и скульптура второй половины XIX века', 26)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (23, N' Живопись и скульптура второй половины XIX века', 20)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (24, N' Живопись и скульптура второй половины XIX века', 2)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (25, N' Живопись и скульптура второй половины XIX века', 14)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (26, N' Живопись и скульптура второй половины XIX века', 33)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (27, N' Живопись и скульптура второй половины XIX века', 34)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (28, N' Живопись и скульптура второй половины XIX века', 7)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (29, N' Живопись и скульптура второй половины XIX века', 46)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (30, N' Живопись и скульптура второй половины XIX века', 35)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (31, N' Живопись и скульптура второй половины XIX века', 44)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (32, N' Живопись и скульптура второй половины XIX века', 28)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (33, N'Врубелевские залы', 49)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (34, N'Врубелевские залы', 22)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (35, N'Врубелевские залы', 13)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (36, N'Живопись и скульптура второй половины XIX века', 47)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (37, N'Живопись и скульптура второй половины XIX века', 11)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (38, N'Живопись и скульптура второй половины XIX века', 18)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (39, N'Живопись и скульптура рубежа ХIХ–ХХ веков', 43)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (40, N'Живопись и скульптура рубежа ХIХ–ХХ веков', 17)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (41, N'Живопись и скульптура рубежа ХIХ–ХХ веков', 45)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (42, N'Живопись и скульптура рубежа ХIХ–ХХ веков', 4)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (43, N'Живопись и скульптура рубежа ХIХ–ХХ веков', 37)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (44, N'Живопись и скульптура рубежа ХIХ–ХХ веков', 32)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (45, N'Живопись и скульптура рубежа ХIХ–ХХ веков', 31)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (46, N'Живопись и скульптура рубежа ХIХ–ХХ веков', 5)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (47, N'Живопись и скульптура рубежа ХIХ–ХХ веков', 15)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (48, N'Живопись и скульптура рубежа ХIХ–ХХ веков', 38)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (49, N'Живопись и скульптура рубежа ХIХ–ХХ веков', 39)
+GO
+INSERT [dbo].[Zali] ([id_zala], [naimenovanie_zala], [kod_otv]) VALUES (50, N'Сокровищница', 9)
+GO
+SET IDENTITY_INSERT [dbo].[Zali] OFF
+GO
